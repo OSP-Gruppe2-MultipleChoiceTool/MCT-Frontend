972c63e42357780ab905546f23cfaa00e043c36a
Phillip Hemleb <github@phillip.hemleb.de>
2025-02-06 13:07:40 +0100

    Added plugins for tailwind and unplug vue components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </main>
 </template>
+
+<script setup lang="ts">
+
+</script>
