b530ea89e118bd627493f74ef20418644b74a1de
zentek-mikemichel <mike.michel@zentek.de>
2025-02-20 11:44:14 +0100

    fix: Fix typo

--- src/utils/api-urls.ts
+++ src/utils/api-urls.ts
@@ -1,13 +1,13 @@
 export abstract class ApiEndpoints {
-    public static readonly QUESTIONNAIRES = '/api/questionnaires';  // Liste aller Fragebögen
-    public static readonly QUESTIONNAIRE_BY_ID = '/api/questionnaires/{questionnaireId}';
-    public static readonly QUESTIONNAIRE_EXPORT = '/api/questionnaires/{questionnaireId}/export';
-    public static readonly QUESTIONNAIRE_LINKS = '/api/questionnaires/{questionnaireId}/links';
-    public static readonly QUESTIONNAIRE_LINK_BY_ID = '/api/questionnaires/{questionnaireId}/links/{linkId}';
-    public static readonly STATEMENT_SETS = '/api/questionnaires/{questionnaireId}/statement-sets';
-    public static readonly STATEMENT_SET_BY_ID = '/api/questionnaires/{questionnaireId}/statement-sets/{statementSetId}';
-    public static readonly STATEMENTS = '/api/questionnaires/{questionnaireId}/statement-sets/{statementSetId}/statements';
-    public static readonly STATEMENT_BY_ID = '/api/questionnaires/{questionnaireId}/statement-sets/{statementSetId}/statements/{statementId}';
+    public static readonly QUESTIONAIRES = '/api/questionaires';  // Liste aller Fragebögen
+    public static readonly QUESTIONAIRE_BY_ID = '/api/questionaires/{questionaireId}';
+    public static readonly QUESTIONAIRE_EXPORT = '/api/questionaires/{questionaireId}/export';
+    public static readonly QUESTIONAIRE_LINKS = '/api/questionaires/{questionaireId}/links';
+    public static readonly QUESTIONAIRE_LINK_BY_ID = '/api/questionaires/{questionaireId}/links/{linkId}';
+    public static readonly STATEMENT_SETS = '/api/questionaires/{questionaireId}/statement-sets';
+    public static readonly STATEMENT_SET_BY_ID = '/api/questionaires/{questionaireId}/statement-sets/{statementSetId}';
+    public static readonly STATEMENTS = '/api/questionaires/{questionaireId}/statement-sets/{statementSetId}/statements';
+    public static readonly STATEMENT_BY_ID = '/api/questionaires/{questionaireId}/statement-sets/{statementSetId}/statements/{statementId}';
     public static readonly STATEMENT_TYPES = '/api/statement-types';
     public static readonly STATEMENT_TYPE_BY_ID = '/api/statement-types/{statementTypeId}';
     public static readonly USER_ACCESS = '/api/statements';
