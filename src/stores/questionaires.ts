16acf89a2e2cef45717fbf390d8264bd66add4a4
zentek-mikemichel <mike.michel@zentek.de>
2025-02-20 13:51:50 +0100

    fix: Fix request in questionaires store

--- src/stores/questionaires.ts
+++ src/stores/questionaires.ts
@@ -1,14 +1,15 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { ApiEndpoints } from '@/utils/api-urls'
+import type { Questionaire } from '@/types/types'
 
 export const useCounterStore = defineStore('questionaires', () => {
-    const questionaires = ref({})
+    const questionaires = ref<Questionaire>()
     const questionairesList = ref([])
 
 
-    async function getQuestionaires(): Promise<void> {
-        if (questionaires !== undefined) return
+    async function createQuestionaires(): Promise<void> {
+        if (questionaires.value !== null) return;
 
         try {
             // TODO Rebuild because of "get-base-url.ts"
@@ -19,7 +20,7 @@ export const useCounterStore = defineStore('questionaires', () => {
                 throw new Error('Failed to Load Questionaires')
             }
 
-            questionaires.value = response
+            questionaires.value = await response.json()
 
         } catch (error) {
             console.error('Failed to load location data:', error)
