2925397372e22467af9189414e47adb2a5e72f10
zentek-mikemichel <mike.michel@zentek.de>
2025-02-20 14:21:37 +0100

    chore: Rebuild create questionaires

--- src/stores/questionaires.ts
+++ src/stores/questionaires.ts
@@ -2,6 +2,7 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { ApiEndpoints } from '@/utils/api-urls'
 import type { Questionaire } from '@/types/types'
+import API from '@/utils/api-wrapper'
 
 export const useCounterStore = defineStore('questionaires', () => {
     const questionaires = ref<Questionaire>()
@@ -12,20 +13,18 @@ export const useCounterStore = defineStore('questionaires', () => {
         if (questionaires.value !== null) return;
 
         try {
-            // TODO Rebuild because of "get-base-url.ts"
-            const response = await fetch(ApiEndpoints.QUESTIONAIRES)
+            const response = await API.post(ApiEndpoints.QUESTIONAIRES)
 
 
-            if (!response.ok) {
-                throw new Error('Failed to Load Questionaires')
-            }
-
-            questionaires.value = await response.json()
+            questionaires.value = await response.data
 
         } catch (error) {
             console.error('Failed to load location data:', error)
 
             throw new Error('Failed to Load Questionaires')
         }
+    }
+    return {
+      createQuestionaires,
     }
 })
