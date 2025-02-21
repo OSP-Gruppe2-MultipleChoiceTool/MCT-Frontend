047019389445bfc811f1b64a95afb6fbf651561b
esalioglou <e.salioglou@neusta.de>
2025-02-21 23:22:02 +0100

    Changes by philliphemleb

--- src/components/ui/SearchbarComponent.vue
+++ src/components/ui/SearchbarComponent.vue
@@ -12,8 +12,8 @@
     value=""
     :rows="5"
     placeholder="Suche..."
-    class="block w-full text-sm h-10 px-4 text-slate-900 bg-gray-100 rounded-[8px] border border-slate-200 appearance-none focus:outline-none
-    -500 overflow-ellipsis overflow-hidden text-nowrap h-full"
+    class="block w-full text-sm h-full px-4 text-slate-900 bg-gray-100 rounded-[8px] border border-slate-200 appearance-none focus:outline-none
+    -500 overflow-ellipsis overflow-hidden text-nowrap"
     type="text"
   />
 </template>
