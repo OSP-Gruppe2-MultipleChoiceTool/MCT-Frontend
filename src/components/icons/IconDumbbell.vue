e9e3565dde4054b88d31ffa9be03bdb5202167f8
emin <emin@duweisst.de>
2025-02-26 21:37:08 +0100

    [FEATURE-ADD]: Mode Selection view

--- src/components/icons/IconDumbbell.vue
+++ src/components/icons/IconDumbbell.vue
@@ -1,6 +1,6 @@
 <template>
-    <svg fill="#000000" height="200px" width="200px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
-        xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24" xml:space="preserve">
+    <svg fill="currentColor" width="1em" height="1em" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve">
         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
         <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
         <g id="SVGRepo_iconCarrier">
@@ -16,5 +16,5 @@
 </template>
 
 <script setup lang="ts">
-
+    
 </script>
--- src/views/ModeSelectView.vue
+++ src/views/ModeSelectView.vue
@@ -1,7 +1,19 @@
 <template>
+    <div class="bg-white flex gap-10 flex justify-center items-center">
+        <div class="bg-gray-50 text-lg w-70 h-30 flex flex-col justify-center items-center rounded-sm border hover:bg-main-blue hover:text-white">
+            <IconDumbbell class="text-3xl" />
+            <p class="font-bold">Übung</p>
+        </div>
+        <div class="bg-gray-50 text-lg w-70 h-30 flex flex-col justify-center items-center rounded-sm border hover:bg-main-blue hover:text-white">
+            <IconClipboard class="text-3xl"/>
+            <p class="font-bold">Prüfen</p>
+        </div>
+    </div>
 
 </template>
-
 <script setup lang="ts">
+import IconClipboard from '@/components/icons/IconClipboard.vue';
+import IconDumbbell from '@/components/icons/IconDumbbell.vue';
+
 
 </script>
