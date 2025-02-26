d49d8cdea232dbc4cade9ea7191caac16b92ce0d
emin <emin@duweisst.de>
2025-02-26 21:43:55 +0100

    traingins view

---
+++ src/views/TrainingModeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <main class="bg-white">
+        <div class="top">
+            btn
+        </div>
+        <div class="card">
+            <div class="left">
+                
+            </div>
+            <div class="right">
+
+            </div>
+        </div>
+
+    </main>
+</template>
+
+<script setup lang="ts">
+
+</script>
