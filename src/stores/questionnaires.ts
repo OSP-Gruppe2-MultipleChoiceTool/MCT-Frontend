9983f6349337a686047923dedf732e5bfce384d6
Phillip Hemleb <github@phillip.hemleb.de>
2025-03-03 15:02:25 +0100

    Update Questionnaire Store

--- src/stores/questionnaires.ts
+++ src/stores/questionnaires.ts
@@ -1,10 +1,11 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import { apiService } from '@/services/apiService.ts'
-import { apiRoutes } from '@/config/apiRoutes.ts'
+import { apiRoutes, buildApiUrl } from '@/config/apiRoutes.ts'
 import type { CreateQuestionnaire, Questionnaire } from '@/types/Questionnaire.ts'
+import { isValidGuid } from '@/composables/useDataValidation.ts'
 
-export const useQuestionnairesStores = defineStore('questionnaires', () => {
+export const useQuestionnairesStore = defineStore('questionnaires', () => {
   const isLoading = ref<boolean>(false);
   const questionnaires = ref<Questionnaire[]>([]);
 
@@ -12,8 +13,19 @@ export const useQuestionnairesStores = defineStore('questionnaires', () => {
     return questionnaires.value;
   }
 
+  const getQuestionnaireById = async (id: string): Promise<Questionnaire|null> => {
+    const questionnaireRoute = buildApiUrl(apiRoutes.questionaireById, { questionaireId: id });
+    const response = await apiService.get<Questionnaire>(questionnaireRoute);
+    if (!response.data) {
+      console.error('No questionnaire found with id ', id);
+      return null;
+    }
+
+    return response.data;
+  }
+
   const createQuestionnaire = async (data: CreateQuestionnaire): Promise<void> => {
-    const response = await apiService.post<{ type: CreateQuestionnaire }>(
+    const response = await apiService.post<CreateQuestionnaire>(
       apiRoutes.questionaires,
       data
     );
@@ -24,8 +36,6 @@ export const useQuestionnairesStores = defineStore('questionnaires', () => {
   }
 
   const fillQuestionnaires = async (): Promise<void> => {
-    isLoading.value = true;
-
     questionnaires.value = [];
 
     const response = await apiService.get<Questionnaire[]>(apiRoutes.questionaires);
@@ -37,9 +47,14 @@ export const useQuestionnairesStores = defineStore('questionnaires', () => {
     response.data.forEach((questionnaire: Questionnaire) => {
       questionnaires.value.push(questionnaire);
     });
-
-    isLoading.value = false;
   };
 
-  return { questionnaires, createQuestionnaire, getQuestionnaires, fillQuestionnaires };
+  return {
+    isLoading,
+    questionnaires,
+    getQuestionnaireById,
+    createQuestionnaire,
+    getQuestionnaires,
+    fillQuestionnaires
+  };
 })
